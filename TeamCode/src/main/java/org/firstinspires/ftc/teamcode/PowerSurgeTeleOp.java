4e5354fff99a18e580b880ea9936c2f7cca48088
Paul Ophoff <ophoff.paul@gmail.com>
2019-09-28 16:25:22 -0500

    Created New TeleOp Program w/ Drive Train Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerSurgeTeleOp.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "PowerSurgeTeleOp")
+@Disabled
+public class PowerSurgeTeleOp extends OpMode {
+
+    public static final double DEADZONE = 0.15;
+
+    private DcMotor FrontRight;
+    private DcMotor FrontLeft;
+    private DcMotor BackRight;
+    private DcMotor BackLeft;
+
+    @Override
+    public void init() {
+        initializeDriveTrain();
+    }
+
+    @Override
+    public void loop() {
+        CheckDriveTrain();
+    }
+
+    //
+    // DRIVE TRAIN
+    //
+
+    public void initializeDriveTrain() {
+        FrontRight = hardwareMap.dcMotor.get("FrontRight");
+        FrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        BackRight = hardwareMap.dcMotor.get("BackRight");
+        BackLeft = hardwareMap.dcMotor.get("BackLeft");
+    }
+
+    public void CheckDriveTrain() {
+        double forwardButton = gamepad1.left_stick_y;
+        double sidewaysButton = gamepad1.left_stick_x;
+        double spinningButton = gamepad1.right_stick_x;
+
+        forwardButton = DeadModifier(forwardButton);
+        sidewaysButton = DeadModifier(sidewaysButton);
+        spinningButton = DeadModifier(spinningButton);
+
+        Drive(forwardButton, sidewaysButton, spinningButton);
+    }
+
+    public double  DeadModifier(double joystickValue) {
+        if(joystickValue < DEADZONE && joystickValue > -DEADZONE)
+            return 0;
+        else {
+            return joystickValue;
+        }
+    }
+
+    public void Drive(double DZForwardButton, double DZSidewaysButton, double DZSpinningButton) {
+        BackRight.setPower(-DZSidewaysButton - DZForwardButton + DZSpinningButton);
+        BackLeft.setPower(-DZSidewaysButton + DZForwardButton + DZSpinningButton);
+        FrontRight.setPower(DZSidewaysButton - DZForwardButton + DZSpinningButton);
+        FrontLeft.setPower(DZSidewaysButton + DZForwardButton + DZSpinningButton);
+    }
+}
