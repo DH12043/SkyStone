eb7695de6149ea6ea45c705f90adfc7efc024efe
ktj100 <kuipert12@gmail.com>
2019-12-31 01:59:44 -0600

    Added library from gluten free, modified by 7571 Alumineers.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/hardware/base/DcMotor.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.lib.hardware.base;
+
+public class DcMotor{
+
+
+  public DcMotor(){
+
+  }
+
+  public void setPower(double power){
+    
+  }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/util/RobotEnums.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.lib.util;
+
+public class RobotEnums {
+/*
+    public enum WheelType {
+        MECANUM, FOUR_WD;
+    }
+
+    public enum ControlType {
+        TANK, ONESTICK_R, ONESTICK_L, FULL_MECANUM, RR1_MECANUM;
+    }
+*/
+}
